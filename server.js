18f2f92c961b97aa444d65ec35f602626341a56a
Christopher Booth <christopher.booth@plymouth.ac.uk>
2021-05-06 12:19:12 +0100

    Completed file upload excercise

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname);
+    }
+  })
+
+var upload = multer({ storage: storage });
+
+var Image = require('./models/Image.js');
 
 var port = 3000;
-var dbURL = //ADD DB URL HERE;
+var dbURL = 'mongodb://localhost:27017/fileUpload';
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -11,10 +25,29 @@ app.use(express.urlencoded({
 }));
 
 app.use(express.static('www'));
+app.use('/uploads', express.static('uploads'));
 
 app.get('/', (req, res)=>{
     res.sendFile('index.html');
 });
+
+app.post('/addPhoto', upload.single('image'), function (req, res, next) {
+    const { title, desc } = req.body;
+    var imageUpload = new Image({
+        title,
+        desc,
+        path: '/uploads/' + req.file.filename
+    })
+    imageUpload.save();
+    res.redirect('/');   
+})
+
+app.get('/getImages', (req, res)=>{
+    Image.find({}, (err, docs)=>{
+        if (err) throw err;
+        res.send(docs);
+    })
+})
 
 mongoose.connect(dbURL, {
     useUnifiedTopology: true,
